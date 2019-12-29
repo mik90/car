4f61995b1e6481879673b61a3fae3ae13ea9f015
mik90 <kaliman.mike@gmail.com>
2019-12-28 20:04:12 -0500

    Playing around with OpenCV tutorials

--- getPiPics.sh
+++ getPiPics.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-# Copies all needed source files to the Pi
-# Really just excludes build stuff and git history
-
-# I'm lazy and have the repo as "Car" on one laptop
-# and "car" on another
-if [[ -d /home/mike/Repos/car/ ]]; then
-	LOCAL_CAR_DIR=/home/mike/Repos/car/
-elif [[ -d  /home/mike/Repos/Car/ ]]; then
-	LOCAL_CAR_DIR=/home/mike/Repos/Car/
+if [[ -d /home/mike/Repos/Car ]]; then
+	LOCAL_CAR_DIR=/home/mike/Repos/Car
 else
 	echo "Unsure where the car repository is, exiting..."
 	exit
 fi
 
 PI_IP_ADDR=192.168.0.169
-REMOTE_CAR_DIR=/home/pi/Repos/car/
+REMOTE_CAR_DIR=/home/pi/Repos/Car/
 
-SOURCE=pi@$PI_IP_ADDR:$REMOTE_CAR_DIR/rpiPics
-DEST=$LOCAL_CAR_DIR
+REMOTE=pi@$PI_IP_ADDR:$REMOTE_CAR_DIR/images
 
 # Copy from remote to local
-rsync -zavh $SOURCE $DEST
+rsync -zavh $REMOTE $LOCAL_CAR_DIR
--- opencv/imageHandler.cpp
+++ opencv/imageHandler.cpp
@@ -3,6 +3,18 @@
 
 // Scratchpad for learning OpenCV
 // Currently looking at https://docs.opencv.org/master/d4/d94/tutorial_camera_calibration.html
+// OpenCV uses 5 distortion parameters as a one-row matrix with 5 columns: k1, k2, p1, p2, k3
+// The camera matrix contains the camera focal length parameters as well as the optical centers
+// expressed as pixel coordinates
+
+// Determing these two matrices is the calibration, OpenCV can use a few different patterns for this
+
+// * Determine distortion matrix
+// * Determine camera matrix
+// * Take input from file list
+// * Read configuration from XML or YAML file
+// * Save configuration into XML or YAML file
+// * Calculate re-projection error
 
 int main(int argc, char** argv)
 {
